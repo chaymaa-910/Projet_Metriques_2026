5e1548368e1b05d7ef34d7a614e566c366294af5
chaymaa-910 <chaymaa.slm91@gmail.com>
2026-05-19 11:56:04 +0000

    Projet métriques terminé

--- app.py
+++ app.py
@@ -8,7 +8,38 @@ def hello_world():
     return render_template('hello.html')
 
 # Déposez votre code à partir d'ici :
+@app.route("/contact")
+def MaPremiereAPI():
+    return render_template("contact.html")
 
+@app.get("/paris")
+def api_paris():
+
+    url = "https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&hourly=temperature_2m"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    times = data.get("hourly", {}).get("time", [])
+
+    temps = data.get("hourly", {}).get("temperature_2m", [])
+
+    n = min(len(times), len(temps))
+
+    result = [
+        {"datetime": times[i], "temperature_c": temps[i]}
+        for i in range(n)
+    ]
+
+    return jsonify(result)
+@app.route("/rapport")
+def mongraphique():
+    return render_template("graphique.html")
+
+@app.route("/histogramme")
+def histogramme():
+    return render_template("histogramme.html")
 
 
 
